288537f773dabacad4e2ad15732be187c2b3f608
Manju23701 <167534096+Manju23701@users.noreply.github.com>
2025-03-10 16:22:31 +0530

    Update register.py

--- Pages/register.py
+++ Pages/register.py
@@ -2,23 +2,25 @@ import time
 
 from selenium.webdriver.common.by import By
 
-
+package="com.smiligence.smiligencehrportal:id/"
 skip = '//android.widget.Button[@content-desc="Skip"]'
 done='//android.widget.Button[@content-desc="done"]'
 reg='//android.view.View[@content-desc="Login/Register"]'
 mobile='//android.widget.EditText[@content-desc="Mobile number"]'
-
-
+password=f"{package}edt_password"
+phone=f"{package}edt_phoneNumber"
 class applyloanclass():
 
     def __init__(self,driver):
         self.driver = driver
 
     def applyloan(self):
-        self.driver.find_element(By.XPATH,skip).click()
+        self.driver.find_element(By.ID,phone).send_keys("9999999999")
         time.sleep(2)
-        self.driver.find_element(By.XPATH, done).click()
+        self.driver.find_element(By.ID, password).send_keys("Welcome@l1")
         time.sleep(2)
+        # self.driver.find_element(By.XPATH, done).click()
+        # time.sleep(2)
         # self.driver.find_element(By.XPATH,reg).click()
         # time.sleep(2)
         # self.driver.find_element(By.XPATH,mobile).click()
