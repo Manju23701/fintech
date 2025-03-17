f58293217d76f60baf0ef5068466c6fde9f73e94
Manju23701 <167534096+Manju23701@users.noreply.github.com>
2025-03-17 11:40:26 +0530

    Update checkin.py

--- Pages/checkin.py
+++ Pages/checkin.py
@@ -3,12 +3,14 @@ import time
 from selenium.webdriver.common.by import By
 
 package="com.smiligence.smiligencehrportal:id/"
-
+allow="com.android.permissioncontroller:id/permission_allow_button"
 checkin_btn=f"{package}checkin_Button"
 camera="com.android.camera2:id/shutter_button"
 done="com.android.camera2:id/done_button"
 checkinconfirm=f"{package}imagecheckin_button"
 allow_permission="com.android.permissioncontroller:id/permission_allow_foreground_only_button"
+
+reason_=f"{package}reason_edittextview"
 checkin_next=f"{package}imagecheckin_button"
 attention=f"{package}attention"
 confirmcheckin=f"{package}conform_checkin_button"
@@ -21,24 +23,27 @@ class checkinclass():
         self.driver = driver
 
     def checkin(self):
+        self.driver.find_element(By.ID, allow).click()
+        time.sleep(4)
         self.driver.find_element(By.ID,checkin_btn).click()
-        time.sleep(2)
+        time.sleep(4)
         self.driver.find_element(By.ID, camera).click()
-        time.sleep(2)
+        time.sleep(4)
         self.driver.find_element(By.ID, done).click()
-        time.sleep(2)
+        time.sleep(4)
         self.driver.find_element(By.ID, checkinconfirm).click()
-        time.sleep(2)
+        time.sleep(4)
         self.driver.find_element(By.ID, allow_permission).click()
-        time.sleep(2)
+        time.sleep(4)
         self.driver.find_element(By.ID, checkin_next).click()
-        time.sleep(2)
-        self.driver.find_element(By.ID, attention).send_keys("yes")
-        time.sleep(2)
-        self.driver.find_element(By.ID, confirmcheckin).click()
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, burger_menu).click()
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, logout).click()
-        time.sleep(2)
+        time.sleep(3)
+        self.driver.find_element(By.ID, reason_).send_keys("Yes")
+        time.sleep(3)
+        # self.driver.find_element(By.ID, attention).send_keys("Yes")
 
+        self.driver.find_element(By.ID, confirmcheckin).click()
+        time.sleep(3)
+        self.driver.find_element(By.XPATH, burger_menu).click()
+        time.sleep(3)
+        self.driver.find_element(By.XPATH, logout).click()
+        time.sleep(3)
