0f1df88a7fbdeb4c8a5b9e08997a8f821b23fb63
Manju23701 <167534096+Manju23701@users.noreply.github.com>
2025-03-18 13:24:03 +0530

    Update register.py

--- Pages/register.py
+++ Pages/register.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class loginClass:
+class loginclass:
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 15)  # Explicit wait for elements
